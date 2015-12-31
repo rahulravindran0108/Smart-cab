013f92f0c94f311eb59f3254852a9fcaba870182
Rahul Ravindran <rahulravindran0108@gmail.com>
2015-12-31 08:52:10 -0800

    feat:adding naive car driving learner

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -11,10 +11,15 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        self.previous_reward = 0
+        self.previous_action = None
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
+        self.previous_reward = 0
+        self.previous_action = None
+        self.state = None
 
     def update(self, t):
         # Gather inputs
@@ -23,14 +28,52 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
+        ## currently using two states called random and initiated
+        if(self.state == None):
+            self.state = 'Random'
+        #print 'environment state:'
+        # {'light': 'green', 'oncoming': None, 'right': None, 'left': None}
+        current_env_state = self.env.sense(self)
+        action = None
+
+        possible_actions = []
+        if(current_env_state['light'] == 'red'):
+            if(current_env_state['oncoming'] != 'left' and current_env_state['left'] != 'forward'):
+                possible_actions = ['right']
+        else:
+            # traffic ligh is gree and now check for oncoming
+            #if no oncoming 
+            if(current_env_state['oncoming'] == 'forward'):
+                possible_actions = [ 'forward','right']
+            else:
+                possible_actions = ['right','forward']
         
         # TODO: Select action according to your policy
-        action = None
+        if possible_actions != [] and self.state == 'Random':
+            action_int =  random.randint(0,len(possible_actions)-1)
+            action = possible_actions[action_int]
+        elif possible_actions != [] and self.state == 'Initiated':
+            action = self.previous_action
+            
+
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
+
+        if(action != None):
+            if(reward > self.previous_reward):
+                self.state = 'Initiated'
+                self.previous_action = action
+                self.previous_reward = reward
+            else:
+                self.state = 'Random'
+                self.previous_action = action
+                self.previous_reward = reward
+
+        
+        
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
